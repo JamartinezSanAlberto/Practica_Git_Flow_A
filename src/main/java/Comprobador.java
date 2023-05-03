14df36370f223a7787348a402cc17fff34ba79aa
Ana Tormo <ana@mail.com>
2023-05-03 10:44:17 +0200

    función comprueba finalizada

---
+++ src/main/java/Comprobador.java
@@ -0,0 +1,13 @@
+public class Comprobador {
+
+
+    public boolean comprueba(String cadena,  int longitud) {
+
+        if (cadena.matches("[A-Z]+") && cadena.length() == longitud) {
+            return true;
+        } else
+        return false;
+        }
+
+
+}
