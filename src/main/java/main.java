4e4e600a949b4b25b0998b25610d94db556e8af6
Oscar Gracia Sanchez <oscar@example.com>
2023-05-10 11:41:22 +0200

    TURNOS FINALIZADO

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -12,13 +12,18 @@ public class main {
         Generador generador = new Generador();
         System.out.println("Escribe una palabra con mayúsculas y 5 letras");
         Scanner sc = new Scanner(System.in);
+        int turnos = 1;
+        System.out.println("Turno: " + turnos);
         String palabra2 = sc.next();
 
         while (!palabra1.equals(palabra2)){
         if (comprobador.comprueba(palabra2, 5)) {
+
             String resultado = generador.generar_pista(palabra1, palabra2);
             System.out.println(resultado);
+            turnos += 1;
             System.out.println("Vuelve a intentarlo");
+            System.out.println("Turno: " + turnos);
             palabra2 = sc.next();
         }
 
