28ec750f117856bedd2bee45ed88d87e40737a6c
Ana Tormo <ana@mail.com>
2023-05-10 11:02:44 +0200

    JUEGO finalizado

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,4 +1,31 @@
+import java.util.Scanner;
+
 public class main {
+    public static void main (String[] args) {
+
+        Creador creador = new Creador();
+        String palabra = creador.crear_clave(5);
+
+        Comprobador comprobador = new Comprobador();
+        Generador generador = new Generador();
+        System.out.println("Escribe una palabra con mayúsculas y 5 letras");
+        Scanner sc = new Scanner(System.in);
+        String palabra2 = sc.next();
+
+        while (!palabra.equals(palabra2)){
+        if (comprobador.comprueba(palabra2, 5)) {
+            String resultado = generador.generar_pista(palabra, palabra2);
+            System.out.println(resultado);
+            System.out.println("Vuelve a intentarlo");
+            palabra2 = sc.next();
+        }
+
+        }
+        System.out.println("Felisitasiones!!!Has acertado la palabra");
+    }
+
+
+
 
 
 
